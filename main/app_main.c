3696f4ec73129cb8efd57d4926cd9fda8184afd4
winxkin <huan2931@gmail.com>
2022-08-11 22:55:40 +0700

    add delay

--- main/app_main.c
+++ main/app_main.c
@@ -6,33 +6,45 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "GPIOconfig.h"
+#include "delay.h"
 
-
-
-
-void input_envent_callback (int pin)
+void vTask1(void * pvParameters )
 {
-   if(pin == GPIO_NUM_0)
-   {
-        static int x =0 ;
-        gpio_set_level(GPIO_NUM_13,x);
-        x = 1-x;       
 
-   }
-
+    while(1)
+    {
+        output_io_toggle(GPIO_NUM_2);
+    }
+    
 }
+
+void vTask2(void * pvParameters )
+{
+
+    while(1)
+    {
+        output_io_toggle(GPIO_NUM_4);
+    }
+    
+}
+
+
 
 void app_main(void)
 {
-    output_io_creat(GPIO_NUM_13);
-    input_io_create(GPIO_NUM_0 , H_to_L);
- 
-    input_set_callback(input_envent_callback);
+    output_io_creat(GPIO_NUM_2);
+    output_io_creat(GPIO_NUM_4);
+    // xTaskCreate(vTask1, "vTask1", 1024, NULL, 4, NULL);
+    // xTaskCreate(vTask2, "vTask2", 1024, NULL, 5, NULL);
+
     while(1)
-    {
-        output_io_toggle(GPIO_NUM_13);
-
+    {   
+       // delay_ms(1);
+        gpio_set_level(2,1);
+        delay_s(1);
+        gpio_set_level(2,0);
+        delay_s(1);
     }
-
+   
     
 }
