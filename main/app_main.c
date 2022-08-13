c1ac0b83b0892efb67fdadbd9a97194bdf66f1e6
winxkin <huan2931@gmail.com>
2022-08-13 16:44:07 +0700

    update uart

--- main/app_main.c
+++ main/app_main.c
@@ -7,44 +7,61 @@
 #include "sdkconfig.h"
 #include "GPIOconfig.h"
 #include "delay.h"
+#include "uart.h"
 
-void vTask1(void * pvParameters )
+void loopTask(void *pvParameters)
 {
-
     while(1)
     {
-        output_io_toggle(GPIO_NUM_2);
+        sentdata(UART_NUM_0,"write serial \n");
+        delay_s(2);
     }
-    
-}
-
-void vTask2(void * pvParameters )
-{
-
-    while(1)
-    {
-        output_io_toggle(GPIO_NUM_4);
-    }
-    
 }
 
 
+
+void cleardata (uint8_t *data, int len)
+{
+    int i;
+    for(i=0;i<len;i++)
+    {
+        *data = 0;
+        data++;
+    }
+}
+
+void checkdata(uint8_t *data,int length)
+{
+    if(data[0] == 0x44 && data[1] == 0x48)
+    {
+        uart_write_bytes(UART_NUM_0,data,length);
+    }
+}
+
+
+void RxTask (void *pvParameters)
+{
+    uint8_t data[128];
+    int length = 0;
+    while(1)
+    {
+        ESP_ERROR_CHECK(uart_get_buffered_data_len(UART_NUM_0, (size_t*)&length));
+        length = uart_read_bytes(UART_NUM_0, data, length, 100);
+        checkdata(data,length);
+        cleardata(data,length);
+    }
+}
+
+void recieveddata()
+{
+    sentdata(UART_NUM_0,"rx serial \n");
+}
 
 void app_main(void)
 {
-    output_io_creat(GPIO_NUM_2);
-    output_io_creat(GPIO_NUM_4);
-    // xTaskCreate(vTask1, "vTask1", 1024, NULL, 4, NULL);
-    // xTaskCreate(vTask2, "vTask2", 1024, NULL, 5, NULL);
-
-    while(1)
-    {   
-       // delay_ms(1);
-        gpio_set_level(2,1);
-        delay_s(1);
-        gpio_set_level(2,0);
-        delay_s(1);
-    }
-   
     
+    start_uart_init(UART_NUM_0);
+    setcallback_uart(recieveddata);
+//    xTaskCreate(loopTask,"lopptask",1024, NULL, 4, NULL);
+    xTaskCreate(RxTask,"RxTask",1024, NULL, 4, NULL);
 }
